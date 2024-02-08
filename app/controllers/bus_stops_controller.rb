3ac230797d54d590c150c33a49eaa561f21dda2d
rizwanpirzada <rizwandev45@gmail.com>
2024-02-08 16:29:43 -0500

    feat: add BusStopsController with index action serving analytics

--- app/controllers/bus_stops_controller.rb
+++ app/controllers/bus_stops_controller.rb
@@ -3,7 +3,6 @@ require 'json'
 
 class BusStopsController < ApplicationController
   def index
-    @longest_route = BusStop.group(:routes).count.max_by { |_, count| count }
-    @most_common_bus_route = BusStop.group(:on_street, :cross_street).count.max_by { |_, count| count }
+    @longest_route = BusStop.longest_route
   end
 end
